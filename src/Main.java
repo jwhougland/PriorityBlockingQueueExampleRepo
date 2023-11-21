2964877857b9e92cd6fbe0bf3bb8474be8391009
jwhougland <47694635+jwhougland@users.noreply.github.com>
2023-11-21 13:17:18 -0600

    Starting threads

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Comparator;
+import java.util.concurrent.PriorityBlockingQueue;
 
 /**
  * Main class for the priority blocking queue console app.
@@ -11,6 +13,17 @@ public class Main {
      */
     public static void main(String[] args) {
 
+        // Creating a priority blocking queue of assignments
+        PriorityBlockingQueue<Assignment> assignmentPriorityQueue = new PriorityBlockingQueue<>(2,
+                Comparator.comparing(Assignment::getDueDate)
+                .thenComparing(Assignment::getPriority));
 
+        // Creating producer and consumer threads
+        Thread producerThread = new Thread(new AssignmentProducer(assignmentPriorityQueue));
+        Thread consumerThread = new Thread(new AssignmentConsumer(assignmentPriorityQueue));
+
+        // Starting the threads
+        producerThread.start();
+        consumerThread.start();
     }
 }
